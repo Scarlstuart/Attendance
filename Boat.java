e2f63ceba0b434ec13904861e8b5bf1494374795
Scarlstuart <134724451+Scarlstuart@users.noreply.github.com>
2023-05-27 00:12:37 +0800

    Add files via upload

---
+++ Boat.java
@@ -0,0 +1,19 @@
+package teach;
+
+public class Boat extends Vehicle {
+	
+	String Name = "Fandango Yacht";
+	String MainSailColor = "White";
+	
+	
+	public void Float() {
+		System.out.println("Driving the Boat!");
+		
+	}
+	void stop() {
+		System.out.println("Stop the vehicle!");
+		
+	}
+	
+
+}
